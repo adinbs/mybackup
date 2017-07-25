60fde4bd4cd5fd2bc763c373cb9d62d362beccf3
adinbs <x3.07.bayuadin@gmail.com>
2017-07-26 06:03:04 +0700

    added script

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+namafiledb=/path/to/your/webroot-$(date +%Y%m%d).sql
+hapusfiledb=/path/to/your/webroot-$(date +%Y%m%d --date="7 days ago").sql
+cd /path/to/your/webroot
+rm -rf $namafiledb
+echo "Backup Last database"
+mysqldump --opt -u dbusername --password="dbpassword" dbname > $namafiledb
+echo "done..."
+git add .
+echo "commit to remote git"
+git commit -am "web update"
+git push -u origin master
+echo "removing last week database file"
+rm -rf $hapusfiledb
